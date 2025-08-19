3528a8f967b788225249e60ddae275ac8e3cc975
kimpler1 <127050021+kimpler1@users.noreply.github.com>
2025-08-19 12:26:42 +0300

    Update and rename main.lua to functions.lua

---
+++ functions.lua
@@ -0,0 +1,3 @@
+-- functions.lua (обновлённый)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kimpler1/script/main/functions_part1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kimpler1/script/main/functions_part2.lua"))()
--- main.lua
+++
@@ -1,6 +0,0 @@
--- Main Loader для Dead Rails GUI
--- Подгружает functions.lua (основной код)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kimpler1/script/main/functions.lua"))()
-
--- Для будущих файлов: добавь строки вроде этой (если нужно extra.lua)
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/kimpler1/script/main/extra.lua"))()
